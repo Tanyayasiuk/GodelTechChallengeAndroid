02aafeef6a6e4aac08757fdfa2a869004e5d6f21
vadzim.salavei <vsalavei@godeltech.com>
2018-12-17 12:34:42 +0300

    Fixed dagger modules hierarchy.

--- app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/ActivityContributorModule.kt
+++ app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/ActivityContributorModule.kt
@@ -1,16 +1,34 @@
 package com.example.tanya.godeltechchallengeandroid.di.module
 
+import android.content.res.Resources
+import com.example.tanya.godeltechchallengeandroid.R
 import com.example.tanya.godeltechchallengeandroid.ui.next.HomeActivity
 import com.example.tanya.godeltechchallengeandroid.ui.start.StartActivity
+import com.example.tanya.godeltechchallengeandroid.ui.start.StartContract
+import com.example.tanya.godeltechchallengeandroid.ui.start.StartPresenter
+import com.example.tanya.godeltechchallengeandroid.util.TimeUnit
+import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.android.ContributesAndroidInjector
 
 @Module
 interface ActivityContributorModule {
 
-    @ContributesAndroidInjector(modules = [UiModule.StartModule::class])
-    fun startActivity (): StartActivity
+    @ContributesAndroidInjector(modules = [StartModule::class])
+    fun startActivity(): StartActivity
 
     @ContributesAndroidInjector
-    fun homeActivity (): HomeActivity
+    fun homeActivity(): HomeActivity
+
+    @Binds
+    fun bindsStartPresenter(startPresenter: StartPresenter): StartContract.Presenter
+
+    @Module
+    class StartModule {
+        @Provides
+        fun provideTimeUnit(resources: Resources): TimeUnit {
+            return TimeUnit(resources.getInteger(R.integer.delay).toLong(), java.util.concurrent.TimeUnit.MILLISECONDS)
+        }
+    }
 }
--- app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/UiModule.kt
+++ app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/UiModule.kt
@@ -1,32 +1,10 @@
 package com.example.tanya.godeltechchallengeandroid.di.module
 
-import android.content.res.Resources
-import com.example.tanya.godeltechchallengeandroid.R
-import com.example.tanya.godeltechchallengeandroid.ui.start.StartContract
-import com.example.tanya.godeltechchallengeandroid.ui.start.StartPresenter
-import com.example.tanya.godeltechchallengeandroid.util.TimeUnit
-import dagger.Binds
 import dagger.Module
-import dagger.Provides
 
-@Module
-class UiModule {
-
-    @Module
-    abstract class StartModule {
-
-        @Module
-        companion object {
-            @Provides
-            @JvmStatic
-            fun provideTimeUnit(resources: Resources): TimeUnit {
-                return TimeUnit(resources.getInteger(R.integer.delay).toLong(), java.util.concurrent.TimeUnit.MILLISECONDS)
-            }
-        }
-
-        @Binds
-        abstract fun provideStartPresenter(startPresenter: StartPresenter): StartContract.Presenter
-
-
-    }
-}
+@Module(
+    includes = [
+        ActivityContributorModule::class
+    ]
+)
+class UiModule
