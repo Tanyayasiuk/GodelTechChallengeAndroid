baf10c021cacc5501b8de40f656827d4976594a0
t.yasiuk <t.yasiuk@gmail.com>
2018-12-26 19:08:15 +0300

    minor dagger fixes;

--- app/src/main/java/com/example/tanya/godeltechchallengeandroid/data/DataModule.kt
+++ app/src/main/java/com/example/tanya/godeltechchallengeandroid/data/DataModule.kt
@@ -2,6 +2,7 @@ package com.example.tanya.godeltechchallengeandroid.data
 
 import android.content.res.Resources
 import com.example.tanya.godeltechchallengeandroid.R
+import com.example.tanya.godeltechchallengeandroid.data.prefs.ApplicationRepositoryImpl
 import com.example.tanya.godeltechchallengeandroid.util.Timespan
 import dagger.Binds
 import dagger.Module
@@ -9,6 +10,9 @@ import dagger.Provides
 
 @Module
 abstract class DataModule {
+
+    @Binds
+    abstract fun bindsToApplicationRepository(applicationRepositoryImpl: ApplicationRepositoryImpl): DataContract.ApplicationRepository
 
     @Binds
     abstract fun bindsToFileRepository(impl: FileRepositoryImpl): DataContract.FileRepository
--- app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/AppModule.kt
+++ app/src/main/java/com/example/tanya/godeltechchallengeandroid/di/module/AppModule.kt
@@ -4,8 +4,6 @@ import android.app.Application
 import android.content.Context
 import android.content.res.Resources
 import com.example.tanya.godeltechchallengeandroid.App
-import com.example.tanya.godeltechchallengeandroid.data.prefs.ApplicationRepository
-import com.example.tanya.godeltechchallengeandroid.data.prefs.ApplicationRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -19,9 +17,6 @@ abstract class AppModule {
 
     @Binds
     abstract fun bindContext(application: Application): Context
-
-    @Binds
-    abstract fun bindApplicationRepository(applicationRepositoryImpl: ApplicationRepositoryImpl): ApplicationRepository
 
     @Module
     companion object {
